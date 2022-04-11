b54e540529d7247f7305876a79f6b8e0709028e7
Vladyslav.Sak <vlsak@deloitte.com>
2022-04-11 23:52:01 +0300

    Added cart context for data

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../context/cart.context';
+
 import Button from '../button/button.component';
 
 import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
 	const { name, price, imageUrl } = product;
+	const { addItemToCart } = useContext(CartContext);
+
+	const addProductToCart = () => addItemToCart(product);
 
 	return (
 		<div className='product-card-container'>
@@ -14,7 +21,9 @@ const ProductCard = ({ product }) => {
 				<span className='price'>{price}</span>
 			</div>
 
-			<Button buttonType='inverted'>Add to card</Button>
+			<Button onClick={addProductToCart} buttonType='inverted'>
+				Add to card
+			</Button>
 		</div>
 	);
 };
