69eeb070fee09ec8cfd6285ef71a5cca661acd07
Vladyslav.Sak <vlsak@deloitte.com>
2022-06-24 13:17:36 +0300

    Added payment functionality

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectCartTotal } from '../../store/cart/cart.selectors';
+import { selectCurrentUser } from '../../store/user/user.selector';
+
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
-import { FormContainer, PaymentFormContainer } from './payment-form.styles';
+import {
+	FormContainer,
+	PaymentFormContainer,
+	PaymentButton,
+} from './payment-form.styles';
 
 const PaymentForm = () => {
 	const stripe = useStripe();
 	const elements = useElements();
+	const amount = useSelector(selectCartTotal);
+	const currentUser = useSelector(selectCurrentUser);
+	const [isProcesingPayment, setIsProcesingPayment] = useState(false);
 
 	const paymentHandler = async (e) => {
 		e.preventDefault();
@@ -15,12 +28,14 @@ const PaymentForm = () => {
 			return;
 		}
 
+		setIsProcesingPayment(true);
+
 		const response = await fetch('/.netlify/functions/create-payment-intent', {
 			method: 'post',
 			headers: {
 				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify({ amount: 1000 }),
+			body: JSON.stringify({ amount: amount * 100 }),
 		}).then((res) => res.json());
 
 		const {
@@ -31,10 +46,12 @@ const PaymentForm = () => {
 			payment_method: {
 				card: elements.getElement(CardElement),
 				billing_details: {
-					name: 'Vlad Sak',
+					name: currentUser ? currentUser.displayName : 'Guest',
 				},
 			},
 		});
+
+		setIsProcesingPayment(false);
 
 		if (paymentResult.error) {
 			alert(paymentResult.error);
@@ -50,7 +67,11 @@ const PaymentForm = () => {
 			<FormContainer onSubmit={paymentHandler}>
 				<h2>Credit card payment:</h2>
 				<CardElement />
-				<Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
+				<PaymentButton
+					isLoading={isProcesingPayment}
+					buttonType={BUTTON_TYPE_CLASSES.inverted}>
+					Pay now
+				</PaymentButton>
 			</FormContainer>
 		</PaymentFormContainer>
 	);
